f5704ba3b021f56f726804fc801802ecc367fedd
James Duot <duot.jim@gmail.com>
2021-05-04 18:46:06 -0700

    removed eval

--- easy1/trinary.rb
+++ easy1/trinary.rb
@@ -4,7 +4,6 @@ class Trinary
   end
 
   def to_decimal
-    puts eval('self', TOPLEVEL_BINDING)
     return 0 if @input =~ /[^0-2]/
     @input.to_i.digits.map.with_index { |el, idx| el * 3 ** idx }.sum
   end
