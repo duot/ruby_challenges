7ded5596953e2eb0ee8e6cb22076e81fa44cb3ef
James Duot <duot.jim@gmail.com>
2021-05-04 19:02:52 -0700

    removed outdated hand test

--- advanced1/rail_fence_cipher_line_test.rb
+++ advanced1/rail_fence_cipher_line_test.rb
@@ -6,10 +6,9 @@ require_relative 'rail_fence_cipher'
 class Line < Minitest::Test
   def test_line_fill_only_content_length_chars
     chars = "abcde".chars
-    line = Line.new(false, 3, 2)
+    line = RailFenceCipher::Zigzag::Line.new(false, 3, 2)
     line.fill! chars
-    binding.pry
-    assert_equal chars, ['d', 'e']
+    assert_equal chars, ['c', 'd', 'e']
   end
 
   def test_line_fill_downwards_only_takes_content_lenght_chars
